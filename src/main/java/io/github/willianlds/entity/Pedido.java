5418e68e1fc524ca7de77098815e8a16adf3f3f6
willian.souza <willian.souza@mjv.com.br>
2022-05-06 17:46:06 -0300

    Migração para o banco MySQL

--- src/main/java/io/github/willianlds/entity/Pedido.java
+++ src/main/java/io/github/willianlds/entity/Pedido.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Table(name = "pedido")
 public class Pedido {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
